73b76ddfd5565bf64d99f7f90dd3acb9b57370a2
ndpower <alexeyvisnevskiy@gmail.com>
2025-02-09 17:07:32 +0200

    Fix weak type enforcement in Mongoose schemas

--- models/order.schema.ts
+++ models/order.schema.ts
@@ -2,11 +2,11 @@ import { Schema } from "mongoose";
 import { Order } from "../interface/IOrder";
 
 export const orderSchema = new Schema<Order>({
-    id: String,
-    issue_date: Date,
-    planned_completion_date: Date,
-    actual_completion_date: Date,
-    work_category: String,
-    car_id: String,
-    mechanic_id: String
+    id: { type: String, required: true },
+    issue_date: { type: Date, required: true },
+    planned_completion_date: { type: Date, required: true },
+    actual_completion_date: { type: Date, required: true },
+    work_category: { type: String, required: true },
+    car_id: { type: String, required: true },
+    mechanic_id: { type: String, required: true },
 })
